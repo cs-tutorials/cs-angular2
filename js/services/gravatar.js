888ab25a3d8efcdd102f57ec38899ec650bad7e1
spiraldancing <spiraldancing@ymail.com>
2016-03-24 12:39:44 -0500

    CS Angular 2 tutorial, 3-4, more Factory-stuff.

---
+++ js/services/gravatar.js
@@ -0,0 +1,8 @@
+angular.module("NoteWrangler")
+.factory("Gravatar", function GravatarFactory(){
+	var avatarSize = 80; // Default size
+	var avatarUrl = "http://www.gravatar.com/avatar/";
+	return function(email){
+		return avatarUrl + CryptoJS.MD5(email) + "?size=" + avatarSize.toString();
+	};
+});
